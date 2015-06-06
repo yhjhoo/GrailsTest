9673017cfbc3d17436ee731e292b137095b8f189
yhjhoo <yhjhoo@163.com>
2015-06-06 13:57:51 +0800

    add templates inside

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
+import me.prince.*
+
 class BootStrap {
 
     def init = { servletContext ->
+		def workTag = new Tag(name: "Work").save(failOnError: true)
+		def homeTag = new Tag(name: "Home").save(failOnError: true)
+		
+		def task = new Task(
+				summary: "Do 2nd intro screencast",
+				details: "Create the second intro screencast for Grails",
+				status: "Open",
+				deadline: new Date())
+		task.addToTags(workTag)
+		task.addToTags(homeTag)
+		task.save(failOnError: true)
     }
     def destroy = {
     }
